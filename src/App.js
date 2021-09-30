44eabb665a2e136140a4796b4d5f545d79592017
Anke <a.stein@posteo.de>
2021-09-30 15:10:59 +0200

    Add Load and Remove buttons

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import './App.css';
 import Header from "./components/Header";
 import characterResponse from "./character-response.json";
 import CharacterGallery from "./components/CharacterGallery";
+import {useState} from "react";
 
 
 function App() {
 
-    const characters = characterResponse.results;
+    const [characters, setCharacters] = useState([])
+
+    const loadCharacters = () => {
+        setCharacters(characterResponse.results)
+    }
+
+    const clearCharacters = () => {
+        setCharacters([])
+    }
+
+
+
 
     return (
-              <div>
-                <Header title="Rick and Morty Gallery"/>
-                <CharacterGallery characters={characters}/>
-            </div>
+        <div>
+            <Header title="Rick and Morty Gallery"/>
+            <button onClick={loadCharacters}>Load characters</button>
+            <button onClick={clearCharacters}>Remove characters</button>
+            <CharacterGallery characters={characters}/>
+        </div>
 
     );
 }
